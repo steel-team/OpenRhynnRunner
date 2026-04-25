e4fa745237deea17e271ca42408a062df6c25bd8
Ivan Yv <steel-97@mail.ru>
2026-04-25 18:49:03 +0300

    fix

--- web/libjs/libui.js
+++ web/libjs/libui.js
@@ -1,11 +1,12 @@
 const getTrueHeight = () => {
-    let dpr = window.devicePixelRatio;
-    let baseSize = window.outerHeight;
-    if(dpr == 1) {
-        baseSize = window.outerHeight / 2;
-    }
-
+    let baseSize = Math.round(window.innerHeight / computeScaleFactor());
     return baseSize;
+};
+
+const computeScaleFactor = () => {
+    let baseSize = window.innerHeight;
+    let targetHeight = 320;
+    return baseSize / targetHeight;
 };
 
 export default {
@@ -14,12 +15,7 @@ export default {
         return input;
     },
     async Java_com_steelteam_EmuNatives_getWidth(lib, baseWidth) {
-        let dpr = window.devicePixelRatio;
-        let baseSize = window.outerWidth;
-        
-        if(dpr == 1) {
-            baseSize = window.outerWidth / 2;
-        }
+        let baseSize = Math.round(window.innerWidth / computeScaleFactor());
 
         const coef = 0.8; //0.75, 240x320 as a base + offset
         let height = getTrueHeight();
